b30563bc98e1114b8469569c05cd08582b1b3883
Nacho <irobertson@damsu.uncuyo.edu.ar>
2025-11-10 13:59:54 -0300

    Actividades m3u4

---
+++ m3u4/ejercicio-2.js
@@ -0,0 +1,17 @@
+const producto = {
+id: 123,
+nombre: "Auriculares inalámbricos",
+precio: 120000,
+stock: 25,
+categoria: "Electrónica"
+};
+
+// Ejercicio a
+const { nombre, precio, stock } = producto;
+
+// Ejercicio b
+console.log("Ejercicio b:");
+console.log("Nombre:", nombre);
+console.log("Precio:", precio);
+console.log("Stock:", stock);
+
